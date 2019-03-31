e6542378030226098e986fb9bdeaf29d843e3e0d
CairoLee <cairoliyu@gmail.com>
2019-03-31 19:43:43 +0800

    Update npc.cpp

--- src/map/npc.cpp
+++ src/map/npc.cpp
@@ -882,11 +882,11 @@ int npc_event_sub(struct map_session_data* sd, struct event_data* ev, const char
 #else
 	enum npce_event current_npce = NPCE_MAX;
 	std::string ename = std::string(eventname), lable;
-	if (ename.find(':')) {
+	if (ename.find(':') != string::npos) {
 		lable = ename.substr(ename.rfind(':') + 1);
 
 		int32 search_i = 0;
-		// ARR_FIND(0, NPCE_MAX, search_i, lable == npc_get_script_event_name(search_i));
+		ARR_FIND(0, NPCE_MAX, search_i, lable == npc_get_script_event_name(search_i));
 		if (search_i != NPCE_MAX)
 			current_npce = (enum npce_event)search_i;
 	}
