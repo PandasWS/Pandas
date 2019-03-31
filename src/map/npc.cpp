5f1e5db50a7006d0df46ee032bae2677f8ab309d
CairoLee <cairoliyu@gmail.com>
2019-03-31 16:51:53 +0800

    尝试修正 LGTM 的一个警告

--- src/map/npc.cpp
+++ src/map/npc.cpp
@@ -885,9 +885,9 @@ int npc_event_sub(struct map_session_data* sd, struct event_data* ev, const char
 	if (ename.find(':') >= 0) {
 		lable = ename.substr(ename.rfind(':') + 1);
 
-		uint16 search_i = 0;
+		int32 search_i = 0;
 		ARR_FIND(0, NPCE_MAX, search_i, lable == npc_get_script_event_name(search_i));
-		if (search_i < NPCE_MAX)
+		if (search_i >= 0 && search_i != NPCE_MAX)
 			current_npce = (enum npce_event)search_i;
 	}
 
