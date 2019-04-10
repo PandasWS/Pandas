432bb3127b621bedc4b8282a4b61b914fdf1e8d1
CairoLee <cairoliyu@gmail.com>
2019-04-10 22:55:02 +0800

    实现与人工生命体相关的五个常规事件 (#96)

--- src/map/npc.cpp
+++ src/map/npc.cpp
@@ -4683,6 +4683,31 @@ const char *npc_get_script_event_name(int npce_index)
 	case NPCE_LEAVE_PARTY:
 		return script_config.leave_party_event_name;	// OnPCPartyLeaveEvent		// 当玩家成功离开队伍后触发事件
 #endif // Pandas_NpcEvent_LEAVE_PARTY
+
+#ifdef Pandas_NpcEvent_HOM_LEVELUP
+	case NPCE_HOM_LEVELUP:
+		return script_config.hom_levelup_event_name;	// OnPCHomLvUpEvent		// 当人工生命体升级时触发事件
+#endif // Pandas_NpcEvent_HOM_LEVELUP
+
+#ifdef Pandas_NpcEvent_HOM_CALL
+	case NPCE_HOM_CALL:
+		return script_config.hom_call_event_name;	// OnPCHomCallEvent		// 当召唤人工生命体时触发事件
+#endif // Pandas_NpcEvent_HOM_CALL
+
+#ifdef Pandas_NpcEvent_HOM_REST
+	case NPCE_HOM_REST:
+		return script_config.hom_rest_event_name;	// OnPCHomRestEvent		// 当人工生命体安息时触发事件
+#endif // Pandas_NpcEvent_HOM_REST
+
+#ifdef Pandas_NpcEvent_HOM_DEAD
+	case NPCE_HOM_DEAD:
+		return script_config.hom_dead_event_name;	// OnPCHomDeadEvent		// 当人工生命体死亡时触发事件
+#endif // Pandas_NpcEvent_HOM_DEAD
+
+#ifdef Pandas_NpcEvent_HOM_WAKE
+	case NPCE_HOM_WAKE:
+		return script_config.hom_wake_event_name;	// OnPCHomAliveEvent		// 当人工生命体复活时触发事件
+#endif // Pandas_NpcEvent_HOM_WAKE
 	// PYHELP - NPCEVENT - INSERT POINT - <Section 6>
 
 	default:
