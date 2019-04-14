43276671570b7d04874c76eba9df89d9a5b740a2
CairoLee <cairoliyu@gmail.com>
2019-04-14 17:51:57 +0800

    实现 OnPCInsertCardFilter 过滤器和 OnPCInsertCardEvent 事件 (#98)

--- src/config/pandas.hpp
+++ src/config/pandas.hpp
@@ -249,6 +249,11 @@
 		// 事件类型: Filter / 事件名称: OnPCGuildLeaveFilter
 		// 常量名称: NPCF_LEAVE_GUILD / 变量名称: leave_guild_filter_name
 		#define Pandas_NpcFilter_LEAVE_GUILD
+
+		// 当玩家准备插入卡片时触发过滤器 [Sola丶小克]
+		// 事件类型: Filter / 事件名称: OnPCInsertCardFilter
+		// 常量名称: NPCF_INSERT_CARD / 变量名称: insert_card_filter_name
+		#define Pandas_NpcFilter_INSERT_CARD
 		// PYHELP - NPCEVENT - INSERT POINT - <Section 1>
 	#endif // Pandas_Struct_Map_Session_Data_EventHalt
 
@@ -330,6 +335,11 @@
 	// 事件类型: Event / 事件名称: OnPCGuildLeaveEvent
 	// 常量名称: NPCE_LEAVE_GUILD / 变量名称: leave_guild_event_name
 	#define Pandas_NpcEvent_LEAVE_GUILD
+
+	// 当玩家成功插入卡片后触发事件 [Sola丶小克]
+	// 事件类型: Event / 事件名称: OnPCInsertCardEvent
+	// 常量名称: NPCE_INSERT_CARD / 变量名称: insert_card_event_name
+	#define Pandas_NpcEvent_INSERT_CARD
 	// PYHELP - NPCEVENT - INSERT POINT - <Section 2>
 #endif // Pandas_NpcEvent
 
