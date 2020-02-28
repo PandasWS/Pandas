2a1aac28f00d18acef9b26d24ed9d2d9f25ac0b1
CairoLee <cairoliyu@gmail.com>
2020-02-29 00:15:38 +0800

    归纳数据库升级脚本以便简化用户的升级操作

---
+++ sql-files/composer/main/upgrade_1.0.2_to_1.0.3_main.sql
@@ -0,0 +1,84 @@
+-- upgrade_20191222.sql
+
+ALTER TABLE `bonus_script`
+    ADD PRIMARY KEY (`char_id`, `type`);
+
+ALTER TABLE `buyingstore_items`
+    ADD PRIMARY KEY (`buyingstore_id`, `index`);
+
+ALTER TABLE `friends`
+    DROP INDEX `char_id`,
+    ADD PRIMARY KEY (`char_id`, `friend_id`);
+
+ALTER TABLE `interlog`
+    ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
+    ADD PRIMARY KEY (`id`),
+    ADD INDEX `time` (`time`);
+
+ALTER TABLE `ipbanlist`
+    DROP INDEX `list`,
+    ADD PRIMARY KEY (`list`, `btime`);
+
+ALTER TABLE `sc_data`
+    DROP INDEX `account_id`,
+    DROP INDEX `char_id`,
+    ADD PRIMARY KEY (`char_id`, `type`);
+
+ALTER TABLE `skillcooldown`
+    DROP INDEX `account_id`,
+    DROP INDEX `char_id`,
+    ADD PRIMARY KEY (`char_id`, `skill`);
+
+ALTER TABLE `vending_items`
+    ADD PRIMARY KEY (`vending_id`, `index`);
+
+DROP TABLE `sstatus`;
+
+-- upgrade_20200108.sql
+
+ALTER TABLE `charlog`
+    -- DROP PRIMARY KEY, -- comment if primary key has not been created yet
+    ADD COLUMN `id` bigint(20) unsigned NOT NULL auto_increment first,
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `account_id` (`account_id`);
+
+-- upgrade_20200109.sql
+
+ALTER TABLE `acc_reg_num`
+	MODIFY `value` bigint(11) NOT NULL default '0';
+
+ALTER TABLE `global_acc_reg_num`
+	MODIFY `value` bigint(11) NOT NULL default '0';
+
+ALTER TABLE `char_reg_num`
+	MODIFY `value` bigint(11) NOT NULL default '0';
+
+-- upgrade_20200126.sql
+
+ALTER TABLE `achievement`
+	MODIFY `count1` int unsigned NOT NULL default '0',
+	MODIFY `count2` int unsigned NOT NULL default '0',
+	MODIFY `count3` int unsigned NOT NULL default '0',
+	MODIFY `count4` int unsigned NOT NULL default '0',
+	MODIFY `count5` int unsigned NOT NULL default '0',
+	MODIFY `count6` int unsigned NOT NULL default '0',
+	MODIFY `count7` int unsigned NOT NULL default '0',
+	MODIFY `count8` int unsigned NOT NULL default '0',
+	MODIFY `count9` int unsigned NOT NULL default '0',
+	MODIFY `count10` int unsigned NOT NULL default '0';
+
+-- upgrade_20200204.sql
+
+ALTER TABLE `guild_member`
+	DROP COLUMN `account_id`,
+	DROP COLUMN `hair`,
+	DROP COLUMN `hair_color`,
+	DROP COLUMN `gender`,
+	DROP COLUMN `class`,
+	DROP COLUMN `lv`,
+	DROP COLUMN `exp_payper`,
+	DROP COLUMN `online`,
+	DROP COLUMN `name`;
+
+ALTER TABLE `friends`
+	DROP COLUMN `friend_account`;
