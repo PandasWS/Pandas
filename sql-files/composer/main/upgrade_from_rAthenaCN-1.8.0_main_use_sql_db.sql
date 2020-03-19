40048dc21c080e3012a6f01d74096275c8aff714
CairoLee <cairoliyu@gmail.com>
2020-03-19 19:47:23 +0800

    添加 rAthenaCN 1.8.0 到熊猫模拟器的数据库升级脚本 (#251)

---
+++ sql-files/composer/main/upgrade_from_rAthenaCN-1.8.0_main_use_sql_db.sql
@@ -0,0 +1,22 @@
+﻿
+-- upgrade_20190628.sql
+
+ALTER TABLE `mob_db`
+	MODIFY `Sprite` varchar(24) NOT NULL,
+	ADD UNIQUE KEY (`Sprite`)
+;
+
+ALTER TABLE `mob_db_re`
+	MODIFY `Sprite` varchar(24) NOT NULL,
+	ADD UNIQUE KEY (`Sprite`)
+;
+
+ALTER TABLE `mob_db2`
+	MODIFY `Sprite` varchar(24) NOT NULL,
+	ADD UNIQUE KEY (`Sprite`)
+;
+
+ALTER TABLE `mob_db2_re`
+	MODIFY `Sprite` varchar(24) NOT NULL,
+	ADD UNIQUE KEY (`Sprite`)
+;
