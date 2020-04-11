0d44215bd67f1c8c43a94b9a7dbc1a922da0b7b5
CairoLee <cairoliyu@gmail.com>
2020-04-11 15:22:00 +0800

    将本次需要建立的数据库独立成升级文档

---
+++ sql-files/composer/main/upgrade_1.0.4_to_1.0.5_main.sql
@@ -0,0 +1,22 @@
+﻿--
+-- Pandas Table structure for table `suspend`
+--
+
+CREATE TABLE IF NOT EXISTS `suspend` (
+  `account_id` int(11) unsigned NOT NULL,
+  `char_id` int(10) unsigned NOT NULL,
+  `sex` enum('F','M') NOT NULL DEFAULT 'M',
+  `map` varchar(20) NOT NULL,
+  `x` smallint(5) unsigned NOT NULL,
+  `y` smallint(5) unsigned NOT NULL,
+  `body_direction` CHAR( 1 ) NOT NULL DEFAULT '4',
+  `head_direction` CHAR( 1 ) NOT NULL DEFAULT '0',
+  `sit` CHAR( 1 ) NOT NULL DEFAULT '1',
+  `mode` tinyint(4) NOT NULL,
+  `tick` bigint(20) NOT NULL default '0',
+  `val1` int(11) NOT NULL default '0',
+  `val2` int(11) NOT NULL default '0',
+  `val3` int(11) NOT NULL default '0',
+  `val4` int(11) NOT NULL default '0',
+  PRIMARY KEY (`account_id`)
+) ENGINE=MyISAM;
