8de67bac3d5ca65bc6bbcac3ff544202f96e8945
CairoLee <cairoliyu@gmail.com>
2020-11-08 16:58:21 +0800

    调整性能计数器的方法使其更清晰

--- src/common/performance.hpp
+++ src/common/performance.hpp
@@ -4,11 +4,15 @@
 #pragma once
 
 #include <string>
+#include "cbasetypes.hpp"
 
-void performance_init(std::string name);
+void performance_create(std::string name);
 void performance_start(std::string name, const char* debug_file = __FILE__, const unsigned long debug_line = __LINE__);
 void performance_stop(std::string name, const char* debug_file = __FILE__, const unsigned long debug_line = __LINE__);
+int64 performance_get_milliseconds(std::string name);
+uint64 performance_get_totalcount(std::string name);
 void performance_report(std::string name, const char* debug_file = __FILE__, const unsigned long debug_line = __LINE__);
+void performance_destory(std::string name);
 
-void performance_begin(std::string name, const char* debug_file = __FILE__, const unsigned long debug_line = __LINE__);
-void performance_end(std::string name, const char* debug_file = __FILE__, const unsigned long debug_line = __LINE__);
+void performance_create_and_start(std::string name, const char* debug_file = __FILE__, const unsigned long debug_line = __LINE__);
+void performance_stop_and_report(std::string name, const char* debug_file = __FILE__, const unsigned long debug_line = __LINE__);
