2909f6862903b85092e76ee64b242c6c2e8ee44c
aleos <aleos89@users.noreply.github.com>
2021-05-11 03:32:29 +0800

    Fixes temp skills not clearing from skill tree (#5931)

--- src/map/clif.cpp
+++ src/map/clif.cpp
@@ -6185,7 +6185,7 @@ void clif_addskill(map_session_data *sd, int skill_id)
 
 /// Deletes a skill from the skill tree (ZC_SKILLINFO_DELETE).
 /// 0441 <skill id>.W
-void clif_deleteskill(map_session_data *sd, int skill_id)
+void clif_deleteskill(map_session_data *sd, int skill_id, bool flag)
 {
 #if PACKETVER >= 20081217
 	nullpo_retv(sd);
@@ -6201,7 +6201,10 @@ void clif_deleteskill(map_session_data *sd, int skill_id)
 	WFIFOW(fd,2) = skill_id;
 	WFIFOSET(fd,packet_len(0x441));
 #endif
-	clif_skillinfoblock(sd);
+#if PACKETVER_MAIN_NUM >= 20190807 || PACKETVER_RE_NUM >= 20190807 || PACKETVER_ZERO_NUM >= 20190918
+	if (!flag)
+#endif
+		clif_skillinfoblock(sd);
 }
 
 /// Updates a skill in the skill tree (ZC_SKILLINFO_UPDATE).
