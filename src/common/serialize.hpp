42cd10db8072c73bf7c30e99efba525637a9d33c
CairoLee <cairoliyu@gmail.com>
2021-08-09 23:17:50 +0800

    使疾风缓存支持 item_randomopt_group.yml 数据库

--- src/common/serialize.hpp
+++ src/common/serialize.hpp
@@ -14,6 +14,7 @@
 #include <boost/archive/binary_iarchive.hpp>
 
 #include <boost/serialization/unordered_map.hpp>
+#include <boost/serialization/map.hpp>
 #include <boost/serialization/shared_ptr.hpp>
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/string.hpp>
