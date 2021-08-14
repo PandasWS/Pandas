4663b23e9f161990be61a79f5cfc66cd5c471a51
CairoLee <cairoliyu@gmail.com>
2021-08-14 11:55:23 +0800

    整理数据库升级脚本汇入 upgrade_20210629.sql 的改动

--- sql-files/composer/main/upgrade_to_1.1.5_main_use_sql_db.sql
+++ sql-files/composer/main/upgrade_to_1.1.5_main_use_sql_db.sql
@@ -6,11 +6,13 @@
 -- ------------------------------------------------------------------------------
 
 -- -----------------------------------------------
--- 熊猫模拟器自定义修改
+-- upgrade_20210629.sql & 熊猫模拟器自定义修改
 -- -----------------------------------------------
 
 -- 将价格字段的类型从 mediumint(8) 提高到 int(10) 以便提高金额上限
 -- 若不调整则最大价格无法超过 8,388,607, 调整后最大可达到 2,147,483,647
+-- 
+-- 将 name_english 长度调整为 100 是 upgrade_20210629.sql 中的修改
 
 DROP PROCEDURE IF EXISTS UPDATE_PANDAS_MAIN_USESQLDB;
 
@@ -19,24 +21,28 @@ CREATE PROCEDURE UPDATE_PANDAS_MAIN_USESQLDB()
 BEGIN
 	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'item_db') THEN
 		ALTER TABLE `item_db`
+			MODIFY `name_english` varchar(100),
 			MODIFY `price_buy` int(10) unsigned default NULL,
 			MODIFY `price_sell` int(10) unsigned default NULL;
 	END IF;
 
 	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'item_db_re') THEN
 		ALTER TABLE `item_db_re`
+			MODIFY `name_english` varchar(100),
 			MODIFY `price_buy` int(10) unsigned default NULL,
 			MODIFY `price_sell` int(10) unsigned default NULL;
 	END IF;
 	
 	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'item_db2') THEN
 		ALTER TABLE `item_db2`
+			MODIFY `name_english` varchar(100),
 			MODIFY `price_buy` int(10) unsigned default NULL,
 			MODIFY `price_sell` int(10) unsigned default NULL;
 	END IF;
 	
 	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'item_db2_re') THEN
 		ALTER TABLE `item_db2_re`
+			MODIFY `name_english` varchar(100),
 			MODIFY `price_buy` int(10) unsigned default NULL,
 			MODIFY `price_sell` int(10) unsigned default NULL;
 	END IF;
