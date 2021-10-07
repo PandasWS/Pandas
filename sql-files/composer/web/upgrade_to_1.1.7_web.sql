2ef07e209a12f09557432740ce590d829542c6be
CairoLee <cairoliyu@gmail.com>
2021-10-07 18:13:41 +0800

    更新数据库升级文件, 将组队招募的相关数据表添加进去

--- sql-files/composer/web/upgrade_to_1.1.7_web.sql
+++ sql-files/composer/web/upgrade_to_1.1.7_web.sql
@@ -9,11 +9,35 @@ ALTER TABLE `char_configs`
 	ADD PRIMARY KEY (`account_id`, `char_id`, `world_name`);
 DELETE FROM `char_configs` WHERE `char_id` = 0;
 
+--
+-- Table structure for table `merchant_configs`
+--
+
 CREATE TABLE IF NOT EXISTS `merchant_configs` (
   `account_id` int(11) unsigned NOT NULL,
   `char_id` INT(11) UNSIGNED NOT NULL,
   `world_name` varchar(32) NOT NULL,
   `store_type` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `data` longtext NOT NULL,
+  PRIMARY KEY (`account_id`, `char_id`, `world_name`)
+) ENGINE=MyISAM;
+
+--
+-- Table structure for table `recruitment`
+--
+
+CREATE TABLE IF NOT EXISTS `recruitment` (
+  `account_id` int(11) unsigned NOT NULL,
+  `char_id` INT(11) UNSIGNED NOT NULL,
+  `char_name` varchar(32) NOT NULL DEFAULT '',
+  `world_name` varchar(32) NOT NULL,
+  `adventure_type` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
+  `tanker` tinyint(3) UNSIGNED NOT NULL DEFAULT 1,
+  `dealer` tinyint(3) UNSIGNED NOT NULL DEFAULT 1,
+  `healer` tinyint(3) UNSIGNED NOT NULL DEFAULT 1,
+  `assist` tinyint(3) UNSIGNED NOT NULL DEFAULT 1,
+  `min_level` INT(11) UNSIGNED NOT NULL,
+  `max_level` INT(11) UNSIGNED NOT NULL,
+  `memo` varchar(32) NOT NULL DEFAULT '',
   PRIMARY KEY (`account_id`, `char_id`, `world_name`)
 ) ENGINE=MyISAM;
