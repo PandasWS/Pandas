97a053d95e410a6d43588f2a16abfd08a7e82e91
kayeeqq <74225264+kayeeqq@users.noreply.github.com>
2021-12-19 18:26:58 +0800

    实现 OnPCDropItemFilter 过滤器, 当玩家准备丢弃或掉落道具时触发 [人鱼姬的思念] (#460)
    Co-authored-by: CairoLee <cairoliyu@gmail.com>

--- src/map/script.cpp
+++ src/map/script.cpp
@@ -363,6 +363,10 @@ struct Script_Config script_config = {
 #ifdef Pandas_NpcFilter_PARTYLEAVE
 	"OnPCPartyLeaveFilter",	// NPCF_PARTYLEAVE		// partyleave_filter_name	// 当玩家准备离开队伍时触发过滤器 [聽風]
 #endif // Pandas_NpcFilter_PARTYLEAVE
+
+#ifdef Pandas_NpcFilter_DROPITEM
+	"OnPCDropItemFilter",	// NPCF_DROPITEM		// dropitem_filter_name	// 当玩家准备丢弃或掉落道具时触发过滤器
+#endif // Pandas_NpcFilter_DROPITEM
 	// PYHELP - NPCEVENT - INSERT POINT - <Section 5>
 
 	/************************************************************************/
