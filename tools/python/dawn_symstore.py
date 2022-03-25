cae7fc435ac5da5ff57a4cd0a795e0c8519af366
CairoLee <cairoliyu@gmail.com>
2022-03-26 00:09:24 +0800

    更新符号归档辅助脚本, 直接将符号上传到指定云存储

--- tools/python/dawn_symstore.py
+++ tools/python/dawn_symstore.py
@@ -18,12 +18,13 @@ import environment
 environment.initialize()
 
 import os
-import git
 import shutil
 import platform
+import oss2
+import glob
 
 from dotenv import load_dotenv
-from libs import Common, Inputer, Message
+from libs import Common, Message
 
 # 切换工作目录为脚本所在目录
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
@@ -33,58 +34,6 @@ project_slndir = '../../'
 
 # 符号仓库工程路径 (在 main 函数中赋值)
 project_symstoredir = ''
-
-def ensure_store():
-    if not Common.is_dir_exists(project_symstoredir):
-        if not Inputer().requireBool({
-            'tips' : '本机尚未克隆最新的符号仓库, 是否立刻进行克隆?',
-            'default' : False
-        }):
-            return False
-
-        # 若环境变量为空则设置个默认值
-        if not os.getenv('DEFINE_REMOTE_SYMBOLS_URL'):
-            os.environ["DEFINE_REMOTE_SYMBOLS_URL"] = "https://github.com/PandasWS/Symbols.git"
-
-        Message.ShowWarning('可能会抛出 UnicodeDecodeError 错误, 请无视即可.')
-        
-        try:
-            git.Repo.clone_from(os.getenv('DEFINE_REMOTE_SYMBOLS_URL'), project_symstoredir)
-        except IndexError as _err:
-            pass
-    return True
-
-def ensure_store_clean():
-    if not Common.is_dir_exists(project_symstoredir):
-        return False
-
-    repo = git.Repo(project_symstoredir)
-    if not repo.is_dirty() and not repo.untracked_files:
-        return True
-
-    if Inputer().requireBool({
-        'tips' : '本地符号仓库的工作区不干净, 是否重置工作区?',
-        'default' : False
-    }):
-        repo.git.reset('--hard')
-        repo.git.clean('-xdf')
-        Message.ShowStatus('已成功重置本地符号仓库到干净状态.')
-        return True
-    return False
-
-def update_store():
-    repo = git.Repo(project_symstoredir)
-    if repo.is_dirty() or repo.untracked_files:
-        return False
-    
-    Message.ShowStatus('正在尝试拉取最新的符号数据, 请稍候...')
-    try:
-        repo.remotes.origin.pull()
-    except git.GitError as _err:
-        Message.ShowWarning('更新失败, 原因: %s' % _err)
-        return False
-    else:
-        return True
 
 def deploy_file(filepath):
     basename = os.path.basename(filepath)
@@ -108,7 +57,10 @@ def deploy_file(filepath):
 def deploy_symbols(sourcedir):
     # 要排除的目录或者文件 (小写)
     exclude_dirs = ['3rdparty', '.vs']
-    exclude_files = ['libmysql.dll', 'dbghelp.dll', 'pcre8.dll', 'zlib.dll']
+    exclude_files = [
+        'libmysql.dll', 'dbghelp.dll', 'pcre8.dll', 'zlib.dll',
+        'vmprotectsdk32.dll', 'vmprotectsdk64.dll'
+    ]
 
     for dirpath, dirnames, filenames in os.walk(sourcedir):
         dirnames[:] = [d for d in dirnames if d.lower() not in exclude_dirs]
@@ -120,26 +72,71 @@ def deploy_symbols(sourcedir):
             fullpath = os.path.join(dirpath, filename)
             deploy_file(fullpath)
 
-def make_commit():
-    try:
-        repo = git.Repo(project_symstoredir)
+def upload_symbols(symstoredir, only_upload_new = False):
+    # 初始化阿里云 OSS
+    auth = oss2.Auth(os.getenv('OSS_SYMBOLS_FULL_ACCESS_KEY_ID'), os.getenv('OSS_SYMBOLS_FULL_ACCESS_KEY_SECRET'))
+    endpoint = os.getenv('OSS_SYMBOLS_ENDPOINT')
+    bucket_name = os.getenv('OSS_SYMBOLS_BUCKET_NAME')
+    bucket = oss2.Bucket(auth, endpoint, bucket_name)
 
-        if not repo.is_dirty() and not repo.untracked_files:
-            Message.ShowWarning('工作区很干净, 没有任何变化文件, 无需提交.')
-            return False
+    if (symstoredir.endswith('/')):
+        symstoredir = symstoredir[:-1]
 
-        repo.git.add('.')
-        repo.git.commit('-m', '归档 {project_name} {version} 版本的符号文件和编译产物, 代码源自 {symbols_source}@{githash} 仓库'.format(
-            project_name = os.getenv('DEFINE_PROJECT_NAME'),
-            symbols_source = os.getenv('DEFINE_SYMBOLS_SOURCE'),
-            version = Common.get_pandas_ver(project_slndir, 'v'),
-            githash = Common.get_pandas_hash(project_slndir)
-        ))
-    except git.GitError as _err:
-        Message.ShowWarning('提交失败, 原因: %s' % _err)
-        return False
+    # 列举本地指定目录下的全部文件    
+    local_files = glob.glob('{symstoredir}/**/*'.format(symstoredir = symstoredir), recursive=True)
+
+    # 只保留文件, 剔除掉目录
+    local_files = [f for f in local_files if os.path.isfile(f)]
+
+    # 移除需要排除的目录下的文件
+    exclude_dir = ['.git']
+    exclude_file = ['.DS_Store']
+    local_files = [f for f in local_files if not any(os.path.sep + d + os.path.sep in f.lower() for d in exclude_dir)]
+    local_files = [f for f in local_files if not any(f.lower().endswith(e) for e in exclude_file)]
+
+    # 准备必要的列表变量用来存储东西
+    all_remote_files = []
+    final_need_upload = []
+
+    # 如果要求只上传本地才有的新文件, 则需要先列举出所有的远程文件
+    if only_upload_new:
+        list_result = bucket.list_objects(prefix = '')
+        while list_result:
+            remote_files = [x.key for x in list_result.object_list]
+            all_remote_files.extend(remote_files)
+            if list_result.is_truncated:
+                list_result = bucket.list_objects(prefix = '', marker = list_result.next_marker)
+            else:
+                break
+        
+        # 此时 all_remote_files 中已经包含了所有的远程文件
+        # 移除其中以 '/' 结尾的信息, 因为他们是目录
+        all_remote_files = [x for x in all_remote_files if not x.endswith('/')]
+
+        # 将 all_remote_files 中的字符串转换成小写
+        all_remote_files = [x.lower() for x in all_remote_files]
+
+        # 从 local_files 中排除 all_remote_files 存在的记录
+        for x in local_files:
+            key = os.path.relpath(x, symstoredir).lower().replace('\\', '/')
+            if key in all_remote_files:
+                Message.ShowInfo('已经跳过: ' + key)
+                continue
+            final_need_upload.append(x)
     else:
-        return True
+        final_need_upload = local_files
+
+    # TODO: 将文件压缩成 cab 以便节省体积
+
+    # 执行文件上传工作
+    for filepath in final_need_upload:
+        key = os.path.relpath(x, symstoredir).lower().replace('\\', '/')
+        Message.ShowInfo('正在上传: {filepath}'.format(filepath = key))
+        try:
+            bucket.put_object_from_file(key, filepath)
+        except Exception as e:
+            Message.ShowError(str(e))
+            pass
 
 def main():
     # 加载 .env 中的配置信息
@@ -162,16 +159,12 @@ def main():
     if not os.getenv('DEFINE_PROJECT_NAME'):
         os.environ["DEFINE_PROJECT_NAME"] = "Pandas"
 
-    if not os.getenv('DEFINE_SYMBOLS_SOURCE'):
-        os.environ["DEFINE_SYMBOLS_SOURCE"] = "PandasWS/Pandas"
-
     if not os.getenv('DEFINE_COMPILE_MODE'):
         os.environ["DEFINE_COMPILE_MODE"] = "re,pre"
     
     # 符号仓库工程路径
     global project_symstoredir
-    project_symstoredir = os.path.abspath(project_slndir + '../Symbols/' + os.getenv('DEFINE_PROJECT_NAME'))
-    
+    project_symstoredir = os.path.abspath(project_slndir + '../Symbols')
     Message.ShowInfo('当前输出的项目名称为: %s' % os.getenv('DEFINE_PROJECT_NAME'))
 
     # 检查是否已经完成了编译
@@ -185,54 +178,15 @@ def main():
             Message.ShowWarning('检测到打包需要的编译产物不完整, 请重新编译. 程序终止.')
             Common.exit_with_pause(-1)
 
-    # 检查符号仓库是否存在, 不存在则创建
-    if not ensure_store():
-        Message.ShowWarning('你放弃了拉取符号仓库, 后续工作无法继续. 程序终止.')
-        Common.exit_with_pause(-1)
-    else:
-        Message.ShowStatus('符号仓库已经存在于本地, 开始确认工作区状态...')
-
-    # 若符号文件存在, 则确保工作区干净
-    if not ensure_store_clean():
-        Message.ShowWarning('为了防止出现意外, 请确保符号仓库的工作区是干净的. 程序终止.')
-        Common.exit_with_pause(-1)
-    else:
-        Message.ShowStatus('很好, 符号仓库的工作区是干净的.')
-    
-    # 尝试更新本地的符号仓库到最新
-    #if not update_store():
-    #    Message.ShowWarning('为了防止出现意外, 请确保符号仓库同步到最新. 程序终止.')
-    #    Common.exit_with_pause(-1)
-    #else:
-    #    Message.ShowStatus('符号仓库已经同步到最新, 开始归档新的符号文件...')
-
     # 搜索工程目录全部 pdb 文件和 exe 文件, 进行归档
     deploy_symbols(project_slndir)
+    Message.ShowStatus('符号文件已经归档完毕, 正在同步到符号服务器...')
     
-    # 若环境变量为空则设置个默认值
-    if not os.getenv('DEFINE_SYMBOLS_COMMIT'):
-        os.environ["DEFINE_SYMBOLS_COMMIT"] = "3"
-    
-    commit_flag = int(os.getenv('DEFINE_SYMBOLS_COMMIT'))
-    
-    # 自动进行 git 提交操作
-    if commit_flag & 1 == 1:
-        if not Common.is_pandas_release(os.path.abspath(project_slndir)):
-            Message.ShowStatus('符号文件已经归档完毕, 但并非正式版所以未自动提交...')
-            Common.exit_with_pause()
+    # 将符号文件归档到符号服务器
+    upload_symbols(project_symstoredir, True)
 
-    if commit_flag & 2 == 2:
-        if Common.md5(os.getenv("DEFINE_CRASHRPT_APPID")) != '952648de2d8f063a07331ae3827bc406':
-            Message.ShowStatus('符号文件已经归档完毕, 但未设置熊猫模拟器官方正式版的崩溃上报 APPID, 所以并未自动提交...')
-            Common.exit_with_pause()
+    Message.ShowStatus('符号文件已经同步完毕.')
 
-    Message.ShowStatus('符号文件已经归档完毕, 正在提交...')
-    if not make_commit():
-        Message.ShowWarning('很抱歉, 提交失败! 请确认失败的原因. 程序终止.')
-        Common.exit_with_pause(-1)
-    else:
-        Message.ShowStatus('提交成功, 请手工推送到远程仓库.')
-    
     # 友好退出, 主要是在 Windows 环境里给予暂停
     Common.exit_with_pause()
 
