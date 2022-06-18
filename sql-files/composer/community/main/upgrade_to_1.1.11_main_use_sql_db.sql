4c8287a36933d9087c0a436aff485d291d1a1666
CairoLee <cairoliyu@gmail.com>
2022-06-18 20:23:08 +0800

    跟随 7d35680, 撰写数据库升级脚本并补充 csv2yaml 的头部信息

--- sql-files/composer/community/main/upgrade_to_1.1.11_main_use_sql_db.sql
+++ sql-files/composer/community/main/upgrade_to_1.1.11_main_use_sql_db.sql
@@ -9,15 +9,32 @@
 -- upgrade_20220224.sql
 -- -----------------------------------------------
 
-ALTER TABLE `mob_db`
-	ADD COLUMN `racegroup_malangdo` tinyint unsigned DEFAULT NULL
-;
-ALTER TABLE `mob_db2`
-	ADD COLUMN `racegroup_malangdo` tinyint unsigned DEFAULT NULL
-;
-ALTER TABLE `mob_db_re`
-	ADD COLUMN `racegroup_malangdo` tinyint unsigned DEFAULT NULL
-;
-ALTER TABLE `mob_db2_re`
-	ADD COLUMN `racegroup_malangdo` tinyint unsigned DEFAULT NULL
-;
+DROP PROCEDURE IF EXISTS UPDATE_PANDAS_MAIN_USESQLDB;
+
+DELIMITER $$
+CREATE PROCEDURE UPDATE_PANDAS_MAIN_USESQLDB()
+BEGIN
+	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'mob_db') THEN
+		ALTER TABLE `mob_db`
+			ADD COLUMN `racegroup_malangdo` tinyint unsigned DEFAULT NULL;
+	END IF;
+	
+	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'mob_db2') THEN
+		ALTER TABLE `mob_db2`
+			ADD COLUMN `racegroup_malangdo` tinyint unsigned DEFAULT NULL;
+	END IF;
+	
+	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'mob_db_re') THEN
+		ALTER TABLE `mob_db_re`
+			ADD COLUMN `racegroup_malangdo` tinyint unsigned DEFAULT NULL;
+	END IF;
+	
+	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'mob_db2_re') THEN
+		ALTER TABLE `mob_db2_re`
+			ADD COLUMN `racegroup_malangdo` tinyint unsigned DEFAULT NULL;
+	END IF;
+END $$
+DELIMITER ;
+
+CALL UPDATE_PANDAS_MAIN_USESQLDB();
+DROP PROCEDURE UPDATE_PANDAS_MAIN_USESQLDB;
