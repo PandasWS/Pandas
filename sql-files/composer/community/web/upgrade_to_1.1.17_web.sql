36e0b6398ea7f52b6a3b2acf75ee9698767c9b07
CairoLee <cairoliyu@gmail.com>
2022-10-29 19:00:39 +0800

    修正 WEB 接口数据库升级脚本的主键错误

--- sql-files/composer/community/web/upgrade_to_1.1.17_web.sql
+++ sql-files/composer/community/web/upgrade_to_1.1.17_web.sql
@@ -17,4 +17,4 @@ ALTER TABLE `char_configs`
 
 ALTER TABLE `merchant_configs`
 	DROP PRIMARY KEY,
-	ADD PRIMARY KEY (`world_name`, `account_id`, `char_id`);
+	ADD PRIMARY KEY (`world_name`, `account_id`, `char_id`, `store_type`);
