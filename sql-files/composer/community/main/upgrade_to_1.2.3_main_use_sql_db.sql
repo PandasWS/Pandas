222c1bc0207e05e370ee1dea30f1a99b041de184
CairoLee <cairoliyu@gmail.com>
2023-12-30 22:39:11 +0800

    将 upgrade_20231201.sql 添加到主数据库升级脚本

--- sql-files/composer/community/main/upgrade_to_1.2.3_main_use_sql_db.sql
+++ sql-files/composer/community/main/upgrade_to_1.2.3_main_use_sql_db.sql
@@ -80,3 +80,41 @@ DELIMITER ;
 
 CALL UPDATE_PANDAS_MAIN_USESQLDB();
 DROP PROCEDURE UPDATE_PANDAS_MAIN_USESQLDB;
+
+-- -----------------------------------------------
+-- upgrade_20231201.sql
+-- -----------------------------------------------
+
+DROP PROCEDURE IF EXISTS UPDATE_PANDAS_MAIN_USESQLDB;
+
+DELIMITER $$
+CREATE PROCEDURE UPDATE_PANDAS_MAIN_USESQLDB()
+BEGIN
+	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'mob_db') THEN
+		ALTER TABLE `mob_db`
+			ADD COLUMN `racegroup_illusion_luanda` tinyint(1) unsigned DEFAULT NULL
+		;
+	END IF;
+	
+	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'mob_db2') THEN
+		ALTER TABLE `mob_db2`
+			ADD COLUMN `racegroup_illusion_luanda` tinyint(1) unsigned DEFAULT NULL
+		;
+	END IF;
+	
+	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'mob_db_re') THEN
+		ALTER TABLE `mob_db_re`
+			ADD COLUMN `racegroup_illusion_luanda` tinyint(1) unsigned DEFAULT NULL
+		;
+	END IF;
+	
+	IF EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = (SELECT DATABASE ()) and TABLE_NAME = 'mob_db2_re') THEN
+		ALTER TABLE `mob_db2_re`
+			ADD COLUMN `racegroup_illusion_luanda` tinyint(1) unsigned DEFAULT NULL
+		;
+	END IF;
+END $$
+DELIMITER ;
+
+CALL UPDATE_PANDAS_MAIN_USESQLDB();
+DROP PROCEDURE UPDATE_PANDAS_MAIN_USESQLDB;
